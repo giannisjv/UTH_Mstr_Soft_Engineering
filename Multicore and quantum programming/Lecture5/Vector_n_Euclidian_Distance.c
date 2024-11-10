9ac418523a4bb10fc16c0ab71ff0c5db0d6428f3
giannisjv <giannisvidras@gmail.com>
2024-11-10 15:20:21 +0200

    vect

--- Multicore and quantum programming/Lecture5/Vector_n_Euclidian_Distance.c
+++ Multicore and quantum programming/Lecture5/Vector_n_Euclidian_Distance.c
@@ -3,16 +3,16 @@
 #include <omp.h>
 #include <time.h>
 #include <math.h>
-#define N 5
+#define N 100000
 
 int main(int argc, char const *argv[])
 {
     int *A = malloc(N * sizeof(int));   // Malloc for dynamic Array A in size of int 4 bytes
     int *B = malloc(N * sizeof(int));   // Malloc for dynamic Array B in size of int 4 bytes
 
-    int i;
+    int i, j;
     srand(time(NULL));
-    double sumA, sumB, cosA, cosB, V_angle, magA, magB, cosine;
+    double sumA, sumB, cosA, cosB, ab, V_angle, magA, magB, cosine;
     sumA = sumB = 0.0;
 
     for(i=0; i<N; i++){
@@ -21,22 +21,25 @@ int main(int argc, char const *argv[])
     }
 
        for(i=0; i<N; i++){
-        printf("\nA: %d  B: %d", A[i], B[i]);     
+        //printf("\nA: %d  B: %d", A[i], B[i]);     
     }
 
     for(i=0; i<N; i++){
-        sumA += A[i] * A[i];            //
+        ab += A[i] * B[i];
+        sumA += A[i] * A[i];
         sumB += B[i] * B[i];
+        
     }
-    printf("\nA: %2.5f  B: %2.5f", sumA, sumB);  
-    magA = sumA * 0.5;                  // Finding the sqrt of sumA (Vector magnitude)
-    magB = sumB * 0.5;                  // Finding the sqrt of sumB (Vector magnitude)
-    printf("\nA: %2.5f  B: %2.5f", magA, magB); 
-    cosine = ((sumA * sumB) / (magA * magB));
-    printf("\ncosine %2.5f",cosine);
-    V_angle = magA * magB * cos(cosine);
+    //printf("\nA: %2.5f  B: %2.5f", sumA, sumB);  
+    magA = sqrt(sumA);                  // Finding the sqrt of sumA (Vector magnitude)
+    magB = sqrt(sumB);                  // Finding the sqrt of sumB (Vector magnitude)
+    //printf("\nA: %2.5f  B: %2.5f Dot Prod: %2.5f", magA, magB, ab); 
+    cosine = ((ab) / (magA * magB));
+    //printf("\ncosine %2.5f",cosine);
 
-    printf("\n%2.10f\n",V_angle);
+    V_angle = magA * magB * cosine;
+
+    printf("\n%2.4f\n",V_angle);
 
 
 
